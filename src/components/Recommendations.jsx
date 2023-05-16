1478a7121473375ebd11747362822350b19a1f69
Patrick <pa3q@outlook.com>
2023-05-16 18:38:03 -0300

    lint fixes

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
 import { Link } from 'react-router-dom';
-import './Recommendations.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import PropTypes from 'prop-types';
